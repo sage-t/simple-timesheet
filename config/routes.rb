d77b68d61acad68ccc413be8db57aca4e4ef2030
Sage Thomas <sage.thomas@outlook.com>
2018-10-29 13:45:35 -0600

    added user accounts and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root 'welcome#index'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
 end
